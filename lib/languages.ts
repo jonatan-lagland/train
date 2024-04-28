a60deb5eab3f77291a58c1f1c3f216415f3fb3a2
jonatan-lagland <jonatan.lagland@gmail.com>
2024-04-28 19:41:24 +0300

    Implement localization options for images and messages

---
+++ lib/languages.ts
@@ -0,0 +1,23 @@
+export const languages = [
+    {
+        id: 'fi',
+        src: '/icons/flag-fi-svgrepo-com.png',
+        alt: 'Suomen kieli',
+        name: 'Suomi',
+        label: 'Valitse kieli'
+    },
+    {
+        id: 'se',
+        src: '/icons/flag-se-svgrepo-com.png',
+        alt: 'Svenska språket',
+        name: 'Svenska',
+        label: 'Välj ett språk'
+    },
+    {
+        id: 'en',
+        src: '/icons/flag-gb-svgrepo-com.png',
+        alt: 'English language',
+        name: 'English',
+        label: 'Select a language'
+    }
+];
