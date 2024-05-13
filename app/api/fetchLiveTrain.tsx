11621d4408b68c6c9f5a0f6d2658ba9416935d89
jonatan-lagland <jonatan.lagland@gmail.com>
2024-05-13 19:55:35 +0300

    Refractor live train data fetching logic

--- app/api/fetchLiveTrain.tsx
+++ app/api/fetchLiveTrain.tsx
@@ -1,12 +1,36 @@
+import { TrainDestination } from "@/components/table/timetable";
 import { Train } from "@/lib/types";
 
-const fetchAmount: number = 10;
+const fetchArrivingAmount: number = 10;
+const fetchArrivedAmount: number = 2;
 
-async function fetchLiveTrain(station: string): Promise<Train[]> {
+type fetchLiveTrainProps = {
+    station: string
+    type: TrainDestination
+}
+
+async function fetchLiveTrain({ station, type }: fetchLiveTrainProps): Promise<Train[]> {
+    const arrivalTrains = `https://rata.digitraffic.fi/api/v1/live-trains/station/${station}?arrived_trains=${fetchArrivedAmount}&arriving_trains=${fetchArrivingAmount}&departed_trains=0&departing_trains=0&include_nonstopping=false&train_categories=Long-distance`;
+    const departingTrains = `https://rata.digitraffic.fi/api/v1/live-trains/station/${station}?arrived_trains=0&arriving_trains=0&departed_trains=${fetchArrivedAmount}&departing_trains=${fetchArrivingAmount}&include_nonstopping=false&train_categories=Long-distance`;
     try {
-        const response = await fetch(`https://rata.digitraffic.fi/api/v1/live-trains/station/${station}?arrived_trains=${fetchAmount}&arriving_trains=${fetchAmount}&departed_trains=0&departing_trains=0&include_nonstopping=false`);
-        const data: Train[] = await response.json();
-        return data;
+        if (type === 'ARRIVAL') {
+            const response = await fetch(
+                arrivalTrains,
+                { next: { revalidate: 30 } }
+            );
+            const data: Train[] = await response.json();
+            return data;
+        }
+        if (type === 'DEPARTURE') {
+            const response = await fetch(
+                departingTrains,
+                { next: { revalidate: 30 } }
+            );
+            const data: Train[] = await response.json();
+            return data;
+        }
+
+        return []; // If neither arrival or departure due to some unknown error, avoid sending incorrect data
     } catch (error) {
         console.error('Error fetching live train data:', error);
         return [];
