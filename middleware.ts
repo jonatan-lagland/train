9818ad8a569fe824e261047f1eb85d7813871970
jonatan-lagland <jonatan.lagland@gmail.com>
2024-07-11 15:56:14 +0300

    Refractor middleware to include additional checks for params on home page and train page

--- middleware.ts
+++ middleware.ts
@@ -5,26 +5,46 @@ export default async function middleware(request: NextRequest) {
     const url = request.nextUrl;
     const params = url.searchParams;
     const pathname = url.pathname;
-    // Regular expression to match paths like /<locale>/<city>
+    // Regular expression to match paths /<locale>/<city>
     const cityPathPattern = /^\/(en|fi|se)\/[^/]+$/;
+
+    let shouldRedirect = false;
 
     // Remove any unwanted parameters
     params.forEach((value, key) => {
         if (key !== 'type' && key !== 'destination') {
             params.delete(key);
+            shouldRedirect = true;
         }
     });
 
-    let shouldRedirect = false;
+    // Remove ALL params for pages that are NOT the city page
+    if (!(cityPathPattern.test(pathname))) {
+        params.forEach((value, key) => {
+            params.delete(key);
+            shouldRedirect = true;
+        });
+    }
+
+    // Applies to city path only
     if (cityPathPattern.test(pathname)) {
+        // Introduce ?type=departure if no params are present
         if (!params.has('type') || (params.get('type') !== 'departure' && params.get('type') !== 'arrival')) {
+            params.set('type', 'departure');
+            shouldRedirect = true;
+        }
+        // Ensure destination trains are always of type departure
+        if (params.has('destination') && params.get('type') !== 'departure') {
             params.set('type', 'departure');
             shouldRedirect = true;
         }
     }
 
-    // Update the URL with the modified search parameters
-    url.search = params.toString();
+    if (shouldRedirect) {
+        // Update the URL with the modified search parameters
+        url.search = params.toString();
+        return NextResponse.redirect(url);
+    }
 
     const handleI18nRouting = createMiddleware({
         locales: ['en', 'fi', 'se'],
@@ -32,11 +52,6 @@ export default async function middleware(request: NextRequest) {
     });
 
     const response = handleI18nRouting(request);
-
-    if (shouldRedirect) {
-        url.search = params.toString();
-        return NextResponse.redirect(url);
-    }
     return response;
 }
 
