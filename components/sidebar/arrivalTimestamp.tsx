e7254943ef6dfc53a9f94a39ba4dc5676ccce345
jonatan-lagland <jonatan.lagland@gmail.com>
2024-07-26 12:18:50 +0300

    Refractor localization for arrival timestamp

--- components/sidebar/arrivalTimestamp.tsx
+++ components/sidebar/arrivalTimestamp.tsx
@@ -3,10 +3,8 @@ import { TimeTable, TrainDestination } from "../table/timetable"
 import { useTranslations } from "next-intl"
 import { SiteLocale } from "@/lib/types"
 
-
-
 type ArrivalTimestampProps = {
-    city: string | string[]
+    city: string
     destinationType: TrainDestination
     locale: SiteLocale
     stationNextTimestamp: string
@@ -24,10 +22,10 @@ type ArrivalTimestampProps = {
  * @param {string} props.stationNextTimestamp - Unix Timestamp of the next train arrival.
  * @param {number} props.stationNextTrainTrack - The train track of the next arriving train.
  * @param {TimeTable[]} props.data - Live train data, the length of which is evaluated.
- * @returns {React.ReactElement}
  */
 export default function ArrivalTimestamp({ city, destinationType, locale, stationNextTimestamp, stationNextTrainTrack, timeStampNow, data }: ArrivalTimestampProps): React.ReactElement {
     const translation = useTranslations('TimeTable');
+    const decodedCity = decodeURIComponent(city)
     const timeDifference = new Date(stationNextTimestamp).getTime() - new Date(timeStampNow).getTime();
     const totalMinutes = Math.max(Math.ceil(timeDifference / 60000), 0); // Ensure the value is not negative with Math.max
     const hours = Math.floor(totalMinutes / 60);
@@ -41,11 +39,11 @@ export default function ArrivalTimestamp({ city, destinationType, locale, statio
     const translated = () => {
         switch (locale) {
             case "se":
-                return <>{destinationType === 'ARRIVAL' ? 'Anländer till' : 'Avgår från'} <span className="capitalize">{city}</span> station om <span className='font-bold'>{travelTime}</span> {destinationType === 'ARRIVAL' ? 'på spår ' : 'från spår '} <span>{stationNextTrainTrack}</span>.</>
+                return <>{destinationType === 'ARRIVAL' ? 'Anländer till' : 'Avgår från'} <span className="capitalize">{decodedCity}</span> station om <span className='font-bold'>{travelTime}</span> {destinationType === 'ARRIVAL' ? 'på spår ' : 'från spår '} <span>{stationNextTrainTrack}</span>.</>
             case "en":
-                return <>{destinationType === 'ARRIVAL' ? 'Arrives at ' : 'Departs from '} <span className="capitalize">{city}</span> station in <span className='font-bold'>{travelTime}</span> {destinationType === 'ARRIVAL' ? 'on track ' : 'from track '} <span>{stationNextTrainTrack}</span>.</>
+                return <>{destinationType === 'ARRIVAL' ? 'Arrives at ' : 'Departs from '} <span className="capitalize">{decodedCity}</span> station in <span className='font-bold'>{travelTime}</span> {destinationType === 'ARRIVAL' ? 'on track ' : 'from track '} <span>{stationNextTrainTrack}</span>.</>
             default:
-                return <>{destinationType === 'ARRIVAL' ? 'Saapuu ' : 'Lähtee '} <span className="capitalize">{city}</span> {destinationType === 'ARRIVAL' ? 'asemalle ' : 'asemalta '} <span className='font-bold'>{travelTime}</span> kuluttua {destinationType === 'ARRIVAL' ? 'raiteelle ' : 'raiteelta '} <span>{stationNextTrainTrack}</span>.</>
+                return <>{destinationType === 'ARRIVAL' ? 'Saapuu ' : 'Lähtee '} <span className="capitalize">{decodedCity}</span> {destinationType === 'ARRIVAL' ? 'asemalle ' : 'asemalta '} <span className='font-bold'>{travelTime}</span> kuluttua {destinationType === 'ARRIVAL' ? 'raiteelle ' : 'raiteelta '} <span>{stationNextTrainTrack}</span>.</>
         }
     };
 
