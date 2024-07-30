b7fead3bcc9354f38720da6f11285706d9b94573
jonatan-lagland <jonatan.lagland@gmail.com>
2024-07-30 16:47:20 +0300

    Fix an issue with arrival timestamp causing hydration errors

--- components/sidebar/arrivalTimestamp.tsx
+++ components/sidebar/arrivalTimestamp.tsx
@@ -28,50 +28,37 @@ export default function ArrivalTimestamp({ city, destinationType, locale, statio
     const translation = useTranslations('TimeTable');
     const decodedCity = decodeURIComponent(city);
 
-    // Function to calculate travel time, wrapped in useCallback
-    const calculateTravelTime = useCallback(() => {
-        const timeDifference = new Date(stationNextTimestamp).getTime() - new Date(timeStampNow).getTime();
-        const totalMinutes = Math.max(Math.ceil(timeDifference / 60000), 0); // Ensure the value is not negative with Math.max
+    const calculateLocalizedLabel = useCallback(() => {
+        const timeDifference = new Date(stationNextTimestamp).getTime() - Date.now();
+        const totalMinutes = Math.max(Math.ceil(timeDifference / 60000), 0);
         const hours = Math.floor(totalMinutes / 60);
         const minutes = totalMinutes % 60;
 
-        return hours > 0
+        const travelTimeLabel = hours > 0
             ? `${hours} ${translation('longHour')} ${minutes} ${translation('longMin')}`
             : `${minutes} ${translation('longMin')}`;
-    }, [stationNextTimestamp, timeStampNow, translation]);
 
-    // State to manage travel time, initial state set using the calculation function
-    const [travelTime, setTravelTime] = useState<string>(calculateTravelTime);
-
-    /*
-        * NOTE:
-        * useEffect is used here because the timestamp rendered on the server is sometimes different
-        * from the one on the client if a minute passes during the time the client is rendered,
-        * which causes a hydration error.
-    */
-    useEffect(() => {
-        // Update the travel time dynamically after the component mounts
-        setTravelTime(calculateTravelTime());
-    }, [calculateTravelTime, stationNextTimestamp, timeStampNow, translation]);
-
-    /* Used due to a variation in word order */
-    const translated = () => {
         switch (locale) {
             case "se":
-                return <>{destinationType === 'ARRIVAL' ? 'Anländer till' : 'Avgår från'} <span className="capitalize">{decodedCity}</span> station om <span className='font-bold'>{travelTime}</span> {destinationType === 'ARRIVAL' ? 'på spår ' : 'från spår '} <span>{stationNextTrainTrack}</span>.</>;
+                return <>{destinationType === 'ARRIVAL' ? 'Anländer till' : 'Avgår från'} <span className="capitalize">{decodedCity}</span> station om <span className='font-bold'>{travelTimeLabel}</span> {destinationType === 'ARRIVAL' ? 'på spår ' : 'från spår '} <span>{stationNextTrainTrack}</span>.</>;
             case "en":
-                return <>{destinationType === 'ARRIVAL' ? 'Arrives at ' : 'Departs from '} <span className="capitalize">{decodedCity}</span> station in <span className='font-bold'>{travelTime}</span> {destinationType === 'ARRIVAL' ? 'on track ' : 'from track '} <span>{stationNextTrainTrack}</span>.</>;
+                return <>{destinationType === 'ARRIVAL' ? 'Arrives at ' : 'Departs from '} <span className="capitalize">{decodedCity}</span> station in <span className='font-bold'>{travelTimeLabel}</span> {destinationType === 'ARRIVAL' ? 'on track ' : 'from track '} <span>{stationNextTrainTrack}</span>.</>;
             default:
-                return <>{destinationType === 'ARRIVAL' ? 'Saapuu ' : 'Lähtee '} <span className="capitalize">{decodedCity}</span> {destinationType === 'ARRIVAL' ? 'asemalle ' : 'asemalta '} <span className='font-bold'>{travelTime}</span> kuluttua {destinationType === 'ARRIVAL' ? 'raiteelle ' : 'raiteelta '} <span>{stationNextTrainTrack}</span>.</>;
+                return <>{destinationType === 'ARRIVAL' ? 'Saapuu ' : 'Lähtee '} <span className="capitalize">{decodedCity}</span> {destinationType === 'ARRIVAL' ? 'asemalle ' : 'asemalta '} <span className='font-bold'>{travelTimeLabel}</span> kuluttua {destinationType === 'ARRIVAL' ? 'raiteelle ' : 'raiteelta '} <span>{stationNextTrainTrack}</span>.</>;
         }
-    };
+    }, [stationNextTimestamp, translation, locale, decodedCity, destinationType, stationNextTrainTrack]);
+
+    const [localizedLabel, setLocalizedLabel] = useState<JSX.Element>(calculateLocalizedLabel);
+
+    useEffect(() => {
+        setLocalizedLabel(calculateLocalizedLabel());
+    }, [calculateLocalizedLabel]);
 
     return (
         <span className='font-medium text-xl text-slate-600'>
             {data.length === 0 ? <span>{translation('noJourneyFound')}</span> : (
-                <>{translated()}</>
+                <>{localizedLabel}</>
             )}
         </span>
     );
-
 }
