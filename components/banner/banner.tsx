35167bec2dbbf9b58c1dee9e9c5c9a6997e38a31
jonatan-lagland <jonatan.lagland@gmail.com>
2024-08-01 13:53:42 +0300

    Refractor timetable page sidebar styling

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -6,9 +6,10 @@ type BannerProps = {
     destinationLabel: BannerLabel
     city: string
     cityDestination?: string
+    isCommuter?: string
 }
 
-export default async function Banner({ destinationLabel, city, cityDestination }: BannerProps) {
+export default async function Banner({ destinationLabel, city, cityDestination, isCommuter }: BannerProps) {
     const cityLabel = decodeURIComponent(city)
     const cityDestinationLabel = cityDestination ? decodeURIComponent(cityDestination) : undefined;
     const t = await getTranslations('TimeTable')
@@ -17,14 +18,17 @@ export default async function Banner({ destinationLabel, city, cityDestination }
         ? 'text-3xl md:text-5xl'
         : 'text-5xl md:text-7xl';
 
+    const headerText = cityDestinationLabel ? `${cityLabel} — ${cityDestinationLabel}` : cityLabel
+    const subheaderText = isCommuter === 'true' ? t('commuterTrains') : t(destinationLabel);
+
     return (
         <div className='flex flex-row gap-8 items-center font-inter px-1 py-6'>
             <div className='flex flex-col gap-1'>
                 <h1 className={`flex-auto font-semibold font-robotoslab text-white capitalize break-words ${labelFontSize}`}>
-                    {cityDestinationLabel ? `${cityLabel} — ${cityDestinationLabel}` : cityLabel}
+                    {headerText}
                 </h1>
                 <h2 className={`text-2xl md:text-4xl font-robotoslab flex-grow font-semibold text-white`}>
-                    {t(`${destinationLabel}`)}
+                    {subheaderText}
                 </h2>
             </div>
         </div>
