87b90f2ac7d79c8245c92be0138cf714435d340c
jonatan-lagland <jonatan.lagland@gmail.com>
2024-09-19 17:27:09 +0300

    Replace favicon design

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,59 +1,60 @@
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider, useMessages } from "next-intl";
 import Nav from "@/components/nav/nav";
 import { Besley, Inter, Roboto_Slab } from "next/font/google";
-import Footer from '@/components/footer/footer';
-import { Analytics } from '@vercel/analytics/react';
+import Footer from "@/components/footer/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const besley = Besley({
-    subsets: ['latin'],
-    weight: ['400', '500', '600'],
-    variable: "--besley",
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--besley",
 });
 const inter = Inter({
-    subsets: ['latin'],
-    variable: "--inter",
+  subsets: ["latin"],
+  variable: "--inter",
 });
 const robotoslab = Roboto_Slab({
-    subsets: ['latin'],
-    weight: ['400', '500', '600'],
-    variable: "--robotoslab",
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--robotoslab",
 });
 
 export default function LocaleLayout({
-    children,
-    params: { locale }
+  children,
+  params: { locale },
 }: {
-    children: React.ReactNode;
-    params: { locale: string };
+  children: React.ReactNode;
+  params: { locale: string };
 }) {
+  const messages = useMessages();
 
-    const messages = useMessages();
-
-    return (
-        <html lang={locale}>
-            <head>
+  return (
+    <html lang={locale}>
+      <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
-            <NextIntlClientProvider messages={messages}>
-                <body className={`${inter.variable} ${besley.variable} ${robotoslab.variable} bg-[#F3F3F3]`}>
-                    {/* Header */}
-                    <header className="flex-row w-full flex shadow-md px-2 justify-evenly bg-white">
-                        <Nav />
-                    </header>
-                    {/* Main */}
-                    <main className=' min-h-lvh'>
-                        {children}
-                        <Analytics />
-                    </main>
-                    <footer className="w-full h-full bg-inherit p-4 shadow-md border-y">
-                        <Footer></Footer>
-                    </footer>
-                </body>
-            </NextIntlClientProvider>
-        </html>
-    );
+      <NextIntlClientProvider messages={messages}>
+        <body
+          className={`${inter.variable} ${besley.variable} ${robotoslab.variable} bg-[#F3F3F3]`}
+        >
+          {/* Header */}
+          <header className="flex-row w-full flex shadow-md px-2 justify-evenly bg-white">
+            <Nav />
+          </header>
+          {/* Main */}
+          <main className=" min-h-lvh">
+            {children}
+            <Analytics />
+          </main>
+          <footer className="w-full h-full bg-inherit p-4 shadow-md border-y">
+            <Footer></Footer>
+          </footer>
+        </body>
+      </NextIntlClientProvider>
+    </html>
+  );
 }
