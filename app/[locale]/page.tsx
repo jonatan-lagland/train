c574c78a27e1da97431d9b3dbccd067577c43597
jonatan-lagland <jonatan.lagland@gmail.com>
2024-11-12 14:04:27 +0200

    Refractor landing page carousel on smaller devices

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,70 +1,115 @@
-import { useLocale, useTranslations } from 'next-intl';
-import Header from '@/components/landing/header';
-import { getTranslations } from 'next-intl/server';
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
-import { Card, CardContent } from '@mui/material';
-import ImageTileContainer, { ImageTile, ImageTileContainerTitle, ImageTileTitle } from '@/components/landing/imageTile';
-import Link from 'next/link';
+import { useLocale, useTranslations } from "next-intl";
+import Header from "@/components/landing/header";
+import { getTranslations } from "next-intl/server";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { Card, CardContent } from "@mui/material";
+import ImageTileContainer, { ImageTile, ImageTileContainerTitle, ImageTileTitle } from "@/components/landing/imageTile";
+import Link from "next/link";
 
 export async function generateMetadata() {
-    const t = await getTranslations('MetaData')
+  const t = await getTranslations("MetaData");
 
-    return {
-        title: `${t('landingTitle')} | Suomilinja`,
-        description: t('description'),
-    };
+  return {
+    title: `${t("landingTitle")} | Suomilinja`,
+    description: t("description"),
+  };
 }
 
 export default function Home() {
-    const locale = useLocale();
-    const t = useTranslations("Landing")
-    const imageTilesTitle = t("imageTilesTitle")
-    const startDelay = 0.0; // Define the starting delay
-    const imageTiles = [
-        { src: 'https://images.unsplash.com/photo-1575810034144-1abb0a8796f2', href: "Helsinki?type=departure&commuter=false", locale: locale, alt: 'Helsinki', title: 'Helsinki' },
-        { src: 'https://images.unsplash.com/photo-1567792213673-cdf2c27dbde2', href: "Espoo?type=departure&commuter=true", locale: locale, alt: 'Espoo', title: 'Espoo' },
-        { src: 'https://images.unsplash.com/photo-1604062604580-dab1c3baf2ae', href: "Tampere?type=departure&commuter=false", locale: locale, alt: 'Tampere', title: 'Tampere' },
-        { src: 'https://images.unsplash.com/photo-1674558211660-4e9e78dc6f36', href: "Oulu?type=departure&commuter=false", locale: locale, alt: 'Oulu', title: 'Oulu' },
-        { src: 'https://images.unsplash.com/photo-1649502482770-ff97033d636e', href: "Turku?type=departure&commuter=false", locale: locale, alt: 'Turku', title: 'Turku' },
-        { src: 'https://images.unsplash.com/photo-1685211412960-0cc656bd868c', href: "Jyväskylä?type=departure&commuter=false", locale: locale, alt: 'Jyväskylä', title: 'Jyväskylä' },
-    ];
+  const locale = useLocale();
+  const t = useTranslations("Landing");
+  const imageTilesTitle = t("imageTilesTitle");
+  const startDelay = 0.0; // Define the starting delay
+  const imageTiles = [
+    {
+      src: "https://images.unsplash.com/photo-1575810034144-1abb0a8796f2",
+      href: "Helsinki?type=departure&commuter=false",
+      locale: locale,
+      alt: "Helsinki",
+      title: "Helsinki",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1567792213673-cdf2c27dbde2",
+      href: "Espoo?type=departure&commuter=true",
+      locale: locale,
+      alt: "Espoo",
+      title: "Espoo",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1604062604580-dab1c3baf2ae",
+      href: "Tampere?type=departure&commuter=false",
+      locale: locale,
+      alt: "Tampere",
+      title: "Tampere",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1674558211660-4e9e78dc6f36",
+      href: "Oulu?type=departure&commuter=false",
+      locale: locale,
+      alt: "Oulu",
+      title: "Oulu",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1649502482770-ff97033d636e",
+      href: "Turku?type=departure&commuter=false",
+      locale: locale,
+      alt: "Turku",
+      title: "Turku",
+    },
+    {
+      src: "https://images.unsplash.com/photo-1685211412960-0cc656bd868c",
+      href: "Jyväskylä?type=departure&commuter=false",
+      locale: locale,
+      alt: "Jyväskylä",
+      title: "Jyväskylä",
+    },
+  ];
 
-    return (
-        <div className="grid grid-rows-[min-content_1fr] gap-16">
-            <Header></Header>
-            <div className='flex flex-col gap-4 py-8'>
-                {/* Mobile devices */}
-                <div className='flex items-center justify-center md:hidden '>
-                    <Carousel className="w-full max-w-sm ">
-                        <CarouselContent>
-                            {imageTiles.map((item, index) => (
-                                <CarouselItem key={index}>
-                                    <div className="p-1">
-                                        <Card>
-                                            <CardContent>
-                                                <Link className='flex relative aspect-square items-center justify-center cursor-pointer' href={`/${locale}/${item.href}`} passHref>
-                                                    <ImageTile src={item.src}></ImageTile>
-                                                    <div className='absolute bottom-8 left-6 text-white'>
-                                                        <ImageTileTitle title={item.title}></ImageTileTitle>
-                                                    </div>
-                                                </Link>
-                                            </CardContent>
-                                        </Card>
-                                    </div>
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-                </div>
-                {/* Desktop devices */}
-                <div className="hidden md:flex flex-row flex-wrap grid-cols-none gap-2 px-2 items-center justify-center w-full">
-                    {imageTiles.map((tile, index) => (
-                        <ImageTileContainer key={index} delay={startDelay + index * 0.4} {...tile} />
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className="grid grid-rows-[min-content_1fr] gap-16">
+      <Header></Header>
+      <div className="flex flex-col gap-4 py-8">
+        {/* Mobile devices */}
+        <div className="flex items-center justify-center md:hidden ">
+          <Carousel
+            opts={{
+              align: "center",
+              loop: false,
+              dragFree: true,
+            }}
+            className="w-full max-w-sm">
+            <CarouselContent>
+              {imageTiles.map((item, index) => (
+                <CarouselItem key={index} className="basis-auto relative">
+                  <div className="p-1">
+                    <Card>
+                      <CardContent className="h-52  w-52">
+                        <Link
+                          className="flex aspect-square items-center justify-center cursor-pointer"
+                          href={`/${locale}/${item.href}`}
+                          passHref>
+                          <ImageTile src={item.src}></ImageTile>
+                          <div className="absolute text-white">
+                            <ImageTileTitle title={item.title}></ImageTileTitle>
+                          </div>
+                        </Link>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </div>
-    );
+        {/* Desktop devices */}
+        <div className="hidden md:flex flex-row flex-wrap grid-cols-none gap-2 px-2 items-center justify-center w-full">
+          {imageTiles.map((tile, index) => (
+            <ImageTileContainer key={index} delay={startDelay + index * 0.4} {...tile} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/landing/imageTile.tsx
+++ components/landing/imageTile.tsx
@@ -1,86 +1,82 @@
-'use client'
+"use client";
 
-import { motion } from "framer-motion"
-import Link from "next/link"
-import Image from 'next/image';
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
 
 type ImageTileContainerProps = {
-    src: string
-    alt: string
-    title: string
-    delay: number
-    locale: string
-    href: string
-}
+  src: string;
+  alt: string;
+  title: string;
+  delay: number;
+  locale: string;
+  href: string;
+};
 
 type ImageTileContainerTitleProps = {
-    delay: number
-    children: React.ReactNode
-}
+  delay: number;
+  children: React.ReactNode;
+};
 
 type ImageTileProps = {
-    src: string
-}
+  src: string;
+};
 
 type ImageTileTitleProps = {
-    title: string
-}
+  title: string;
+};
 
 export const ImageTile = ({ src }: ImageTileProps) => {
-    return (
-        <div className="relative w-full h-full transition-transform duration-300 transform group-hover:scale-105">
-            <Image
-                src={src}
-                priority
-                fill
-                quality={10}
-                style={{ objectFit: 'cover' }}
-                alt={''}
-                sizes="
+  return (
+    <div className="relative w-full h-full transition-transform duration-300 transform group-hover:scale-105">
+      <Image
+        src={src}
+        priority
+        fill={true}
+        quality={10}
+        style={{ objectFit: "cover" }}
+        alt={""}
+        sizes="
                     (max-width: 640px) 700px,
                     (max-width: 768px) 850px,
                     550px"
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-transparent rounded-lg"></div>
-        </div>
-    );
+      />
+      <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-transparent rounded-lg"></div>
+    </div>
+  );
 };
 
-
 export const ImageTileTitle = ({ title }: ImageTileTitleProps) => {
-    return <h3 className="text-2xl font-robotoslab font-semibold">{title}</h3>
-}
+  return <h3 className="text-2xl font-robotoslab font-semibold">{title}</h3>;
+};
 
 export const ImageTileContainerTitle = ({ children, delay }: ImageTileContainerTitleProps) => (
-    <motion.div
-        className={`text-4xl font-semibold font-robotoslab`}
-        initial={{ opacity: 0, }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2, delay }}
-    >
-        {children}
-    </motion.div>
+  <motion.div
+    className={`text-4xl font-semibold font-robotoslab`}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 2, delay }}>
+    {children}
+  </motion.div>
 );
 
 const ImageTileContainer = ({ src, alt, title, delay, locale, href }: ImageTileContainerProps) => (
-    <Link href={`/${locale}/${href}`} passHref>
-        <motion.div
-            className={`relative h-44 md:h-64 w-full md:w-48 cursor-pointer overflow-hidden group rounded-lg`}
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.4, delay }}
-        >
-            <ImageTile src={src}></ImageTile>
-            <motion.div
-                className="absolute bottom-4 left-4 text-white"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: delay + 0.2 }}
-            >
-                <ImageTileTitle title={title}></ImageTileTitle>
-            </motion.div>
-        </motion.div>
-    </Link>
+  <Link href={`/${locale}/${href}`} passHref>
+    <motion.div
+      className={`relative h-44 md:h-64 w-full md:w-48 cursor-pointer overflow-hidden group rounded-lg`}
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1.4, delay }}>
+      <ImageTile src={src}></ImageTile>
+      <motion.div
+        className="absolute bottom-4 left-4 text-white"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: delay + 0.2 }}>
+        <ImageTileTitle title={title}></ImageTileTitle>
+      </motion.div>
+    </motion.div>
+  </Link>
 );
 
 export default ImageTileContainer;
